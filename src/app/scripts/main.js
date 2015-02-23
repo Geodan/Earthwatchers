bacf6b85215e36cc728bcf8e44097539e5c9bd47
bertt <btemme@hotmail.com>
2015-02-23 14:33:01 +0100

    initial

---
+++ src/app/scripts/main.js
@@ -0,0 +1,75 @@
+/*jslint browser: true*/
+/*global L */
+
+(function (window, document, L, undefined) {
+	'use strict';
+
+	L.Icon.Default.imagePath = 'images/';
+
+	/* create leaflet map */
+	var center = [0.0, 113];
+	var map = L.map('map', {
+		center: [center[0], center[1]],
+		zoom: 7
+	});
+
+    var esri = new L.esri.basemapLayer('Imagery');
+    //L.esri.basemapLayer('ImageryLabels').addTo(map);
+    L.tileLayer('http://geodan.blob.core.windows.net/satellite/LS8_26oktober2014/{z}/{x}/{y}.png', {tms:true}).addTo(map);
+    var ggl2 = new L.Google('satellite');
+	map.addLayer(ggl2);
+	map.addControl(new L.Control.Layers( {'Google':ggl2, 'Esri':esri}, {}, {collapsed:false}));
+	    // Options for the hexbin layer
+	/**
+	var options = {
+	    radius : 10000,                            // Size of the hexagons/bins
+	    opacity: 0.5,                           // Opacity of the hexagonal layer
+	    duration: 200,                          // millisecond duration of d3 transitions (see note below)
+	    lng: 113,       // longitude accessor
+	    lat: 0,       // latitude accessor
+	    value: 3, // value accessor - derives the bin value
+	    valueFloor: 0,                          // override the color scale domain low value
+	    valueCeil: undefined,                   // override the color scale domain high value
+	    colorRange: ['#f7fbff', '#08306b']      // default color range for the heat map
+	};
+
+	// Create the hexbin layer and add it to the map
+	var hexLayer = L.hexbinLayer(options).addTo(map);
+	//hexLayer.data([[113, 0]]);
+    */
+    var options = {
+    radius : 12,
+    opacity: 0.5,
+    duration: 200,
+    lng: function(d){
+        return d[0];
+    },
+    lat: function(d){
+        return d[1];
+    },
+    value: function(d){
+        return d.length;
+    },
+    valueFloor: 0,
+    valueCeil: undefined
+};
+
+var hexLayer = L.hexbinLayer(options).addTo(map);
+hexLayer.colorScale().range(['white', 'blue']);
+
+var latFn = d3.random.normal(center[0], 1);
+var longFn = d3.random.normal(center[1], 1);
+
+var generateData = function(){
+    var data = [];
+    for(var i=0; i<1000; i++){
+        data.push([longFn(),  latFn()]);
+    }
+    hexLayer.data(data);
+};
+generateData();
+
+
+    // http://geodan.blob.core.windows.net/satellite/USGS_LS8_False_North_24june'13/10/830/514.png
+
+}(window, document, L));
---
+++ src/server.js
@@ -0,0 +1,33 @@
+// Get the packages we need
+var express = require('express');
+var path = require('path');
+
+// Create our Express application
+var app = express();
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'Earthwatchers serverside' });
+});
+
+router.get('/version', function(req, res) {
+  res.json({ message: 'Version 0.2' });
+});
+
+
+// Register all our routes with /api
+app.use('/api', router);
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'app')));
+
+
+// Start the server
+app.listen(port);
+console.log('Earthwatchers started on port ' + port);
