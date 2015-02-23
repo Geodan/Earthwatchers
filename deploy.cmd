41d2b5d07fa333af20ba6d1fce50bb30d258b36d
bertt <btemme@hotmail.com>
2015-02-23 17:35:16 +0100

    dsd

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,8 @@
-
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 0.1.5
+:: Version: 0.1.11
 :: ----------------------
 
 :: Prerequisites
@@ -24,11 +23,7 @@ setlocal enabledelayedexpansion
 SET ARTIFACTS=%~dp0%..\artifacts
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%src
-)
-
-IF NOT DEFINED DEPLOYMENT_DIST (
-  SET DEPLOYMENT_DIST=%~dp0%dist\prod
+  SET DEPLOYMENT_SOURCE=%~dp0%.
 )
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
@@ -46,25 +41,12 @@ IF NOT DEFINED NEXT_MANIFEST_PATH (
 IF NOT DEFINED KUDU_SYNC_CMD (
   :: Install kudu sync
   echo Installing Kudu Sync
-  call npm --registry "http://registry.npmjs.org/" install kudusync -g
+  call npm install kudusync -g --silent
   IF !ERRORLEVEL! NEQ 0 goto error
 
   :: Locally just running "kuduSync" would also work
-  SET KUDU_SYNC_CMD=node "%appdata%\npm\node_modules\kuduSync\bin\kuduSync"
+  SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
 )
-
-IF NOT DEFINED GRUNT_CMD (
-  :: Install grunt
-  echo Installing Grunt
-  call npm --registry "http://registry.npmjs.org/" install grunt-cli
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  :: Locally just running "grunt" would also work
-  SET GRUNT_CMD=node "%appdata%\npm\node_modules\grunt-cli\bin\grunt"
-
-)
-
-
 goto Deployment
 
 :: Utility Functions
@@ -74,14 +56,14 @@ goto Deployment
 
 IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
   :: The following are done only on Windows Azure Websites environment
-  call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
+  call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%\src" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
   IF !ERRORLEVEL! NEQ 0 goto error
 
   IF EXIST "%DEPLOYMENT_TEMP%\__nodeVersion.tmp" (
     SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
   )
-
+  
   IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
     SET /p NPM_JS_PATH=<"%DEPLOYMENT_TEMP%\__npmVersion.tmp"
     IF !ERRORLEVEL! NEQ 0 goto error
@@ -93,7 +75,7 @@ IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
 
   SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
 ) ELSE (
-  SET NPM_CMD=npm --registry "http://registry.npmjs.org/"
+  SET NPM_CMD=npm
   SET NODE_EXE=node
 )
 
@@ -105,37 +87,20 @@ goto :EOF
 
 :Deployment
 echo Handling node.js deployment.
-echo %DEPLOYMENT_SOURCE%
 
-:: 1. Select node version
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\src" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: 2. Select node version
 call :SelectNodeVersion
 
-:: 2. Install npm packages
-echo Installing npm dev dependendencies
-if EXIST "%DEPLOYMENT_SOURCE%\package.json" (
-  pushd %DEPLOYMENT_SOURCE%
-  echo Cleaning NPM cache.
-  call !NPM_CMD! cache clean
-  call !NPM_CMD! install --development
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-:: 3. Run grunt prod task
-pushd %DEPLOYMENT_SOURCE%
-call !GRUNT_CMD! prod
-popd
-
-:: 4. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call %KUDU_SYNC_CMD% -v 50 -f "%DEPLOYMENT_DIST%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-:: 5. Install npm packages
+:: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd %DEPLOYMENT_TARGET%
-  call !NPM_CMD! install --production
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
@@ -143,12 +108,21 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
-call %POST_DEPLOYMENT_ACTION%
+IF DEFINED POST_DEPLOYMENT_ACTION call "%POST_DEPLOYMENT_ACTION%"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 goto end
 
+:: Execute command routine that will echo out when error
+:ExecuteCmd
+setlocal
+set _CMD_=%*
+call %_CMD_%
+if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
+exit /b %ERRORLEVEL%
+
 :error
+endlocal
 echo An error has occurred during web site deployment.
 call :exitSetErrorLevel
 call :exitFromFunction 2>nul
@@ -160,4 +134,5 @@ exit /b 1
 ()
 
 :end
+endlocal
 echo Finished successfully.
