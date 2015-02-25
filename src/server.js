f16ecf1c137b20b320677a28181200087571f5b9
bertt <btemme@hotmail.com>
2015-02-25 17:32:31 +0100

    added satelliteimages service and logging

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-// Get the packages we need
 var express = require('express');
 var path = require('path');
 var dotenv = require('dotenv');
+var fs = require('fs');
+var turf = require('turf');
+var morgan = require('morgan');
 
-// Create our Express application
+
+var expressLogFile = fs.createWriteStream(__dirname + '/express.log', {flags: 'a'});
+
 var app = express();
+app.use(morgan('combined', {stream: expressLogFile}))
 dotenv.load();
-console.log("environment: " + process.env.SENDGRID_USERNAME);
 
-// Use environment defined port or 3000
 var port = process.env.PORT || 3000;
-
-// Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing
-// http://localhost:3000/api
 router.get('/', function(req, res) {
+  // todo: return root document
   res.json({ message: 'Earthwatchers serverside' });
 });
 
@@ -24,13 +24,32 @@ router.get('/version', function(req, res) {
   res.json({ message: 'Version 0.2' });
 });
 
+// spatial select the stalliteimages that intersect with client envelope
+router.get('/satelliteimages', function(req, res) {
+	// todo: add parameters
+	var polygonClient = turf.polygon([[
+		[ 110.45623613600009, -0.947576260999938 ], 
+		[ 110.45623613600009, 0.941824488000066 ], 
+		[ 112.6727473310001, 0.941824488000066 ], 
+		[ 112.6727473310001, -0.947576260999938 ], 
+		[ 110.45623613600009, -0.947576260999938 ]
+	]]);
 
-// Register all our routes with /api
+    var satelliteimages = fs.readFileSync('satelliteimages.geojson','utf8');
+    var jsonSatelliteImages = JSON.parse(satelliteimages);
+    var selectedSatelliteImages = [];
+    for (var f in jsonSatelliteImages.features) {
+    	var intersection = turf.intersect(jsonSatelliteImages.features[f], polygonClient);
+    	if(intersection!=null)
+    	{
+    		selectedSatelliteImages.push(jsonSatelliteImages.features[f]);
+    	}
+	}
+	res.json(turf.featurecollection(selectedSatelliteImages));
+  }
+);
+
 app.use('/api', router);
-// app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'app')));
-
-
-// Start the server
 app.listen(port);
-console.log('Earthwatchers started on port ' + port);
+console.log('Earthwatchers server started on port ' + port);
