6d528a2a614308e4e8d4286a0a0499f406e1eca2
bertt <btemme@hotmail.com>
2015-02-26 15:57:38 +0100

    add heohex

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -8,35 +8,32 @@
 
 	/* create leaflet map */
 	var center = [0.0, 113];
+	var lat = 0.153115115481276;
+	var lon = 111.687242798354;
+	var geohexcode = "PO2670248";
+	var zone = GEOHEX.getZoneByCode(geohexcode);
+
 	var map = L.map('map', {
-		center: [center[0], center[1]],
+		center: [lat, lon],
 		zoom: 7
 	});
 
-    var esri = new L.esri.basemapLayer('Imagery');
-    //L.esri.basemapLayer('ImageryLabels').addTo(map);
-    L.tileLayer('http://geodan.blob.core.windows.net/satellite/LS8_26oktober2014/{z}/{x}/{y}.png', {tms:true}).addTo(map);
-    var ggl2 = new L.Google('satellite');
-	map.addLayer(ggl2);
-	map.addControl(new L.Control.Layers( {'Google':ggl2, 'Esri':esri}, {}, {collapsed:false}));
-	    // Options for the hexbin layer
-	/**
-	var options = {
-	    radius : 10000,                            // Size of the hexagons/bins
-	    opacity: 0.5,                           // Opacity of the hexagonal layer
-	    duration: 200,                          // millisecond duration of d3 transitions (see note below)
-	    lng: 113,       // longitude accessor
-	    lat: 0,       // latitude accessor
-	    value: 3, // value accessor - derives the bin value
-	    valueFloor: 0,                          // override the color scale domain low value
-	    valueCeil: undefined,                   // override the color scale domain high value
-	    colorRange: ['#f7fbff', '#08306b']      // default color range for the heat map
+	var myStyle = {
+    "color": "#ff0000",
+    "weight": 5,
+    "opacity": 0.65
 	};
 
-	// Create the hexbin layer and add it to the map
-	var hexLayer = L.hexbinLayer(options).addTo(map);
-	//hexLayer.data([[113, 0]]);
-    */
+	var myhex = L.polygon(zone.getHexCoords(),myStyle);
+
+	//map.fitBounds(myhex.getBounds());
+	// minlevel 0, maxlevel 13
+    L.tileLayer('http://geodan.blob.core.windows.net/satellite/LS8_26oktober2014/{z}/{x}/{y}.png', {tms:true, maxZoom:13}).addTo(map);
+    var ggl2 = new L.Google('satellite');
+	map.addLayer(ggl2);
+	omnivore.topojson('project.topojson').addTo(map);
+	map.addLayer(myhex);
+	// map.addControl(new L.Control.Layers( {'Google':ggl2, 'Esri':esri}, {}, {collapsed:false}));
     var options = {
     radius : 12,
     opacity: 0.5,
@@ -54,21 +51,9 @@
     valueCeil: undefined
 };
 
-var hexLayer = L.hexbinLayer(options).addTo(map);
-hexLayer.colorScale().range(['white', 'blue']);
 
-var latFn = d3.random.normal(center[0], 1);
-var longFn = d3.random.normal(center[1], 1);
 
-var generateData = function(){
-    var data = [];
-    for(var i=0; i<1000; i++){
-        data.push([longFn(),  latFn()]);
-    }
-    hexLayer.data(data);
-};
-generateData();
-omnivore.topojson('project.topojson').addTo(map);
+
 
 
     // http://geodan.blob.core.windows.net/satellite/USGS_LS8_False_North_24june'13/10/830/514.png
