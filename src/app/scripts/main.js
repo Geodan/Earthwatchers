c6dbf3719f81ef55f8d6ff3e9a55f35c3bb729d7
bertt <btemme@hotmail.com>
2015-03-02 18:12:46 +0100

    retrieve satelliteimages based on bbox and type

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,17 +1,36 @@
 /*jslint browser: true*/
 /*global L */
 
+function getSatelliteImageData(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+            callback(data);
+        }
+    };
+    request.send();
+}
+
+function satelliteImagescallback(req) {
+    alert('response:' + req.features.length);
+}
+
 (function (window, document, L, undefined) {
-	'use strict';
+    'use strict';
 
 	L.Icon.Default.imagePath = 'images/';
+    
 
 	/* create leaflet map */
 	var center = [0.0, 113];
 	var lat = 0.153115115481276;
 	var lon = 111.687242798354;
-	var geohexcode = "PO2670248";
-	var zone = GEOHEX.getZoneByCode(geohexcode);
+    var geohexcode = "PO2670248";
+    
+    var zone = GEOHEX.getZoneByCode(geohexcode);
 
 	var map = L.map('map', {
 		center: [lat, lon],
@@ -25,7 +44,13 @@
 	};
 
 	var myhex = L.polygon(zone.getHexCoords(),myStyle);
+    var bounds = myhex.getBounds();
+    getSatelliteImageData('api/satelliteimages?bbox=111.68114616674286, 0.14783529583901883, 111.69333942996492, 0.158394933823336&imagetype=Aerial', satelliteImagescallback);
+ 
+	//alert(bounds.toBBoxString());
+    
 
+    
 	//map.fitBounds(myhex.getBounds());
 	// minlevel 0, maxlevel 13
     L.tileLayer('http://geodan.blob.core.windows.net/satellite/LS8_26oktober2014/{z}/{x}/{y}.png', {tms:true, maxZoom:13}).addTo(map);
@@ -50,12 +75,5 @@
     valueFloor: 0,
     valueCeil: undefined
 };
-
-
-
-
-
-
-    // http://geodan.blob.core.windows.net/satellite/USGS_LS8_False_North_24june'13/10/830/514.png
 
 }(window, document, L));
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var turf = require('turf');
 var morgan = require('morgan');
 
+var imageTypes = { 'EVI': 0, 'Infrared': 1, 'TrueColor': 2, 'Aerial': 3, 'SAR': 4 };
 
 var expressLogFile = fs.createWriteStream(__dirname + '/express.log', {flags: 'a'});
 
@@ -25,27 +26,36 @@ router.get('/version', function(req, res) {
 });
 
 // spatial select the stalliteimages that intersect with client envelope
-router.get('/satelliteimages', function(req, res) {
-	// todo: add parameters
-	var polygonClient = turf.polygon([[
-		[ 110.45623613600009, -0.947576260999938 ], 
-		[ 110.45623613600009, 0.941824488000066 ], 
-		[ 112.6727473310001, 0.941824488000066 ], 
-		[ 112.6727473310001, -0.947576260999938 ], 
-		[ 110.45623613600009, -0.947576260999938 ]
-	]]);
+// example request: /api/satelliteimages?bbox=111.68114616674286,0.14783529583901883,111.69333942996492,0.158394933823336&imagetype=Aerial
+router.get('/satelliteimages', function (req, res) {
+    var bboxPar = req.query.bbox;
+    var imageType = req.query.imagetype;
+    if (bboxPar != null || imageType!=null) {
+        var imageTypeNr =  imageTypes[imageType];
+        
+        var bbox = bboxPar.split(',');
+        for (var i = 0, len = bbox.length; i < len; i++) {
+            bbox[i] = parseFloat(bbox[i]);
+        }
+        var poly = turf.bboxPolygon(bbox);
+        console.log('poly: ' + poly);
 
-    var satelliteimages = fs.readFileSync('satelliteimages.geojson','utf8');
-    var jsonSatelliteImages = JSON.parse(satelliteimages);
-    var selectedSatelliteImages = [];
-    for (var f in jsonSatelliteImages.features) {
-    	var intersection = turf.intersect(jsonSatelliteImages.features[f], polygonClient);
-    	if(intersection!=null)
-    	{
-    		selectedSatelliteImages.push(jsonSatelliteImages.features[f]);
-    	}
-	}
-	res.json(turf.featurecollection(selectedSatelliteImages));
+        var satelliteimages = fs.readFileSync('satelliteimages.geojson', 'utf8');
+        var jsonSatelliteImages = JSON.parse(satelliteimages);
+        var selectedSatelliteImages = [];
+        for (var f in jsonSatelliteImages.features) {
+            var intersection = turf.intersect(jsonSatelliteImages.features[f], poly);
+            if (intersection != null) {
+                if (jsonSatelliteImages.features[f].properties.ImageType == imageTypeNr) {
+                    selectedSatelliteImages.push(jsonSatelliteImages.features[f]);
+                }
+            }
+        }
+        res.json(turf.featurecollection(selectedSatelliteImages));
+
+    } else {
+        res.status(404).send('Error: Parameter bbox not specified');
+    }
   }
 );
 
