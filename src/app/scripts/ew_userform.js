a30da40fa82cb03b6d9e52fae35d1b1be029f8d9
MartijnHeuff <martijn.heuff@geodan.nl>
2015-05-18 15:07:23 +0200

    Keep the total number of hexagons in the user statistics when a username changes...

--- src/app/scripts/ew_userform.js
+++ src/app/scripts/ew_userform.js
@@ -16,5 +16,11 @@ function updateUserInfo() {
         '<span class="username"> Hi, ' + user + '!</span> ' +
             '<a class="userhint" onclick="showUserForm();">Change?</a>';
 
-    loadUserStatistics(projectName, user);
+    loadUserStatistics(projectName, user, getTotalHexagonsFromStatistics());
+}
+
+function getTotalHexagonsFromStatistics() {
+    var text = document.getElementById('hexagonstatistics').innerHTML;
+    var start = text.indexOf('of ') + 3;
+    return +text.substr(start);
 }
